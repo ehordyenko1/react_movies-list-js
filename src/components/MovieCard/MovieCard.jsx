e48a1aa38dbb4b1fade21ecf846f530b935562ba
Yehor Gordiienko <egordienko319@gmail.com>
2024-12-19 22:19:00 +0300

    func is done

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,34 +1,29 @@
-import './MovieCard.scss';
-
 export const MovieCard = ({ film }) => (
-  <div className="card" data-cy="movies-container">
+  <div className="card" data-cy="Movie">
     <div className="card-image">
       <figure className="image is-4by3">
         <img data-cy="MovieImage" src={film.imgUrl} alt="Film logo" />
       </figure>
     </div>
-
     <div className="card-content">
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
+            <img src={film.imgUrl} alt="imdb" />
           </figure>
         </div>
-
         <div className="media-content">
           <p className="title is-8" data-cy="MovieTitle">
             {film.title}
           </p>
         </div>
       </div>
-    </div>
-    <div className="content">
-      <p data-cy="MovieDescription">{film.description}</p>
-
-      <a href={film.imdbUrl} data-cy="MovieLink">
-        IMDB
-      </a>
+      <div className="content">
+        <p data-cy="MovieDescription">{film.description}</p>
+        <a href={film.imdbUrl} data-cy="MovieLink">
+          IMDB
+        </a>
+      </div>
     </div>
   </div>
 );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import './MovieList.scss';
 export const MovieList = ({ films }) => (
   <div className="films" data-cy="movies-container">
     {films.map(film => (
-      <MovieCard key={film.imdbId} movie={film} />
+      <MovieCard key={film.imdbId} film={film} />
     ))}
   </div>
 );
